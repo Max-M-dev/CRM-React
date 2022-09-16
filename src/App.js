15c5c06a6ff60fb18f5fdfb2b54e2f2cd5faa2fc
Max <admin@example.com>
2022-09-16 14:42:49 +0300

    destructuring html into blocks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+import Header from "./component/section/header";
+import Sidebar from "./component/section/sidebar";
+import Footer from "./component/section/footer";
+import "./style/style.css";
+import Index from "./page";
+
 function App() {
-	return;
+	return (
+		<div className="container-scroller">
+			<Header />
+			<div className="container-fluid page-body-wrapper">
+				<Sidebar />
+				<div className="main-panel">
+					<div className="content-wrapper">
+						<Index />
+					</div>
+					{/* <!-- content-wrapper ends --> */}
+					{/* <!-- partial:partials/_footer.html --> */}
+					<Footer />
+					{/* <!-- partial --> */}
+				</div>
+				{/* <!-- main-panel ends --> */}
+			</div>
+			{/* <!-- page-body-wrapper ends --> */}
+		</div>
+	);
 }
 
 export default App;
